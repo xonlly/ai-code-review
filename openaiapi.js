70ad4190ce8e6f18dbe13097e8dab4eab7f9fe1c
JDucellier <jeanducellier.dev@gmail.com>
2023-07-20 14:32:43 +0200

    fix(F): line number

--- openaiapi.js
+++ openaiapi.js
@@ -161,7 +161,7 @@ class OpenAIAPI {
               "fileCommenter:",
               args.reviewCommentFromAIExpert,
               args.fileName,
-              args.lineNumber + 1
+              args.lineNumber
             );
             this.addFunctionResult(
               "addReviewCommentToFileLine",
@@ -173,7 +173,7 @@ class OpenAIAPI {
             await this.fileCommenter(
               args.reviewCommentFromAIExpert,
               args.fileName,
-              args.lineNumber + 1
+              args.lineNumber
             );
             retries = 0;
             continue;
